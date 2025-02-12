cecb8adcb8eaf8869182ca0a7e9d2a166944968d
kevit-devanshi-kadecha <devanshi.kadecha@kevit.io>
2025-02-12 12:41:05 +0530

    Add Bootstrap for styling and implement Navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AuthModule } from './auth/auth.module';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
