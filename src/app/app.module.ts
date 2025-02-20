fffbe7bbab1a0bf3f9b45b839184f9bed1dde7de
kevit-devanshi-kadecha <devanshi.kadecha@kevit.io>
2025-02-20 18:42:50 +0530

    Add AppRoutingModule and update Dashboard routing to include path

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NavbarComponent } from './shared/navbar/navbar.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeModule } from './home/home.module';
 import { DashboardModule } from './dashboard/dashboard.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { DashboardModule } from './dashboard/dashboard.module';
   imports: [
     BrowserModule,
     RouterModule.forRoot([]),
+    AppRoutingModule,
     HttpClientModule,
     AuthModule,
     HomeModule,
