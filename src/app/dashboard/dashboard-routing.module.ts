57e8a4a015e5bcd731a339388f2c734b35a81ee7
kevit-devanshi-kadecha <devanshi.kadecha@kevit.io>
2025-02-25 19:01:01 +0530

    included charts and widgets in dashboard

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
+import { VehicleStatusChartComponent } from './charts/vehicle-status-chart/vehicle-status-chart.component';
+import { RecentVehiclesWidgetComponent } from './widgets/recent-vehicles-widget/recent-vehicles-widget.component';
 
 const routes: Routes = [
-  { path: '', component: DashboardComponent }
+  { path: '', component: DashboardComponent, children: [
+    { path: 'charts/vehicle-status', component: VehicleStatusChartComponent },
+    { path: 'charts/vehicles-byfuel', component: VehicleStatusChartComponent },
+    { path: 'widgets/recent-vehicles', component: RecentVehiclesWidgetComponent },
+    { path: 'widgets/total-vehicles', component: RecentVehiclesWidgetComponent }
+  ] 
+}
 ];
 
 @NgModule({
